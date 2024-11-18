eecb2e84fe8d34587160fed63e59582d0e8c8a5a
szima.akos <indoorkechup@gmail.com>
2024-11-18 09:15:52 +0100

    Add project files.

---
+++ ForumMotor/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ForumMotor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumMotor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ ForumMotor/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ForumMotor.Models
+{
+    public class User : IdentityUser
+    {
+        string? VezetekNev { get; set; }
+        string? KeresztNev { get; set; }
+
+
+    }
+}
